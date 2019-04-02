812e22664270244acf11db2388920eb8d00345cf
edpetrosyan <edpetrosyan@usa.com>
2019-04-03 01:05:40 +0400

    Add Group Method

--- Grouping/Program.cs
+++ Grouping/Program.cs
@@ -10,7 +10,22 @@ namespace Grouping
     {
         static void Main(string[] args)
         {
-
+            string text = "barev axpers vonc es inch ka";
+            Group(text);
         }
+
+        static void Group(string text)
+        {
+            string[] splitedText = text.Split(' ');
+            var result = splitedText.OrderBy(x => x.Length)
+                .Select(x=> x)
+                .ToList();
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
     }
 }
